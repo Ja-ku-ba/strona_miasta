f9a74d66a77d8e1cc81b3843f725ee739b8bfc24
hp <wulfrykpierozek@gmial.com>
2022-12-26 17:27:26 +0100

    district rename

--- places/urls.py
+++ places/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('places_forms', views.places_forms, name='places_forms')
+    path('places_forms', views.places_forms, name='places_forms'),
+
+    
+    path('district_list', views.district_list, name='district_list'),
+    path('district_form/<str:pk>', views.district, name='district_form'),
 ]
--- places/views.py
+++ places/views.py
@@ -1,5 +1,26 @@
-from django.shortcuts import render
+#django
+from django.shortcuts import render, redirect
+
+#self made
+from .forms import DistrictForm
+from .models import District
 
 # Create your views here.
 def places_forms(request):
     return render(request, 'places/places_forms.html')
+
+def district_list(request):                                                    #try to make district add/rename without this function
+    districts = District.objects.all()
+    context = {'districts':districts}
+    return render(request, 'places/forms/districts_list.html', context)
+
+def district(request, pk):
+    district = District.objects.get(id=pk)
+    form = DistrictForm()
+    if request.method == "POST":
+        new_name = request.POST.get('name')
+        district.name = new_name
+        district.save()
+        return redirect('/miejsca/district_list')
+    context = {'form':form}
+    return render(request, 'places/forms/district_form.html', context)
