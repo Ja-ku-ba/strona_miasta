488600aa64fbb5e4b3e243f161643bf79d117d02
hp <wulfrykpierozek@gmail.com>
2023-01-13 11:37:45 +0100

    better register view

--- notifications/models.py
+++ notifications/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+from users.models import Account
+from posts.models import Post
+from message.models import MessagesRoom, RoomDeleteAsk
+
+from places.models import LocalRating, LocalProductRating
 # Create your models here.
+
+
+class UserNotifications(models.Model):
+    user = models.ManyToManyField(Account)
+    posts = models.ManyToManyField(Post)
+    messages = models.ManyToManyField(MessagesRoom)
+    messages_room_ask = models.ManyToManyField(RoomDeleteAsk)
+
+
+
+
--- starter/settings.py
+++ starter/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    "rest_framework",
+    'rest_framework',
 
     'core',
     'users',
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'posts',
     'message',
     'api',
+    'notifications',
 ]
 
 MIDDLEWARE = [
