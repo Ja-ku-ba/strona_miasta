e3972ce83e51654ba8d521d18aa73d67730ecc27
hp <wulfrykpierozek@gmail.com>
2023-01-20 14:09:51 +0100

    messages managment changed

--- message/urls.py
+++ message/urls.py
@@ -2,10 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('chat/<str:pk>', views.create_room, name='create_room'),
-    path('rozmowa', views.messages_list, name='chat_list'),
+    path('stwórz/rozmowa/<str:pk>', views.create_room, name='create_room'),
+    path('rozmowy', views.messages_list, name='chat_list'),
+    path('rozmowa/<int:pk>', views.room, name='chat'),
     
     path('wyslij_wiadomosc/<str:room_id>/<str:user_id>', views.message_add, name='message_add'),
 
-    path('usun_chat/<str:second_user_id>', views.ask, name='delete_chat')
+    path('usun_chat/<str:room_id>', views.ask, name='delete_chat')
 ]
--- message/views.py
+++ message/views.py
@@ -11,17 +11,28 @@ def create_room(request, pk):
     user_1 = Account.objects.get(id=request.user.id)                                               #user who wnants to talk, and creates room
     user_2 = Account.objects.get(id=pk)                                                            #user to who user1 wants to tallk
     
-    if MessagesRoom.objects.filter(owner1=user_1, owner2=user_2).exists():
-        room = MessagesRoom.objects.get(owner1=user_1, owner2=user_2)                              #redirects user to chat room, if that exists
-    elif MessagesRoom.objects.filter(owner1=user_2, owner2=user_1).exists():
+    if MessagesRoom.objects.filter(owner1=user_1, owner2=user_2).exists():                         #redirects user to chat room, if that exists
+        room = MessagesRoom.objects.get(owner1=user_1, owner2=user_2)    
+        if room is True:
+            return redirect('chat', room.id)                          
+    elif MessagesRoom.objects.filter(owner1=user_2, owner2=user_1).exists():                        
         room = MessagesRoom.objects.get(owner1=user_2, owner2=user_1)
-    else:
-        room = MessagesRoom.objects.create(
-            owner1 = user_1,
-            owner2 = user_2
-        )
-    messages_users = room.message_set.all()
-    context = {'messages_users':messages_users, 'room':room, 'user_1':user_1}
+        if room is True:
+            return redirect('chat', room.id)
+    
+    # print(room) ------------check if room exixts, if yes then redirect to room pages
+
+    room = MessagesRoom.objects.create(
+        owner1 = user_1,
+        owner2 = user_2
+    )
+    return redirect('chat', room.id)
+
+@login_required
+def room(request, pk):
+    room = MessagesRoom.objects.get(id=pk)
+    messages_users = Message.objects.filter(room=room)
+    context = {'room':room, 'messages_users':messages_users}
     return render(request, 'message/chat.html', context)
 
 @login_required
@@ -35,8 +46,8 @@ def message_add(request, room_id, user_id):
         body = request.POST.get('body')
     )
     if users.owner1.id != request.user.id:
-        return redirect('create_room', users.owner1.id)
-    return redirect('create_room', users.owner2.id)
+        return redirect('chat', room.id)
+    return redirect('chat', room.id)
 
 @login_required
 def messages_list(request):
@@ -46,35 +57,43 @@ def messages_list(request):
     context = {'user_messages_rooms':user_messages_rooms}
     return render(request, 'message/chat_list.html', context)
 
+
 @login_required
-def ask(request, second_user_id):
-    user_second = Account.objects.get(id=second_user_id)
-    try:
-        room_req = MessagesRoom.objects.get(owner1=request.user, owner2=user_second)
-        ask_exists = RoomDeleteAsk.objects.filter(room=room_req).exists()
-        if ask_exists is False:
-            RoomDeleteAsk.objects.create(
-                user1_ask = request.user,
-                room = room_req
-            )
-            messages.info(request, f'{user_second} otrzyma wiadomość z zapytanie o potwierdzenie usunięcia chatu')
-        else:
-            room_req.delete()
-            RoomDeleteAsk.objects.get(room = room_req).delete()
-            messages.info(request, 'Chat został pomyślnie usunięty')
+def ask(request, room_id):
+    room = MessagesRoom.objects.get(id=room_id)
+    ask = RoomDeleteAsk.objects.filter(room=room)
+    #define second user
+    if request.user != room.owner1:
+        second_user = room.owner1
+    else:
+        second_user = room.owner2
+
+    #if one user deleted an account
+    if second_user is None:
+        room.delete()
+        messages.info(request, 'Chat został pomyślnie usunięty')
         return redirect('chat_list')
 
-        
-    except:
-        room_req = MessagesRoom.objects.get(owner1=user_second, owner2=request.user)
-        ask_exists = RoomDeleteAsk.objects.filter(room=room_req).exists()
-        if ask_exists is False:
-            RoomDeleteAsk.objects.create(
-                user1_ask = request.user,
-                room = room_req
-            )
-        else:
-            room_req.delete()
-            RoomDeleteAsk.objects.get(room = room_req).delete()
+    if ask.exists():
+        #chek is second user confirms, or first spam the button
+        delete_confirmation = RoomDeleteAsk.objects.get(room=room)
+        if delete_confirmation.user1_ask == second_user:
+            room.delete()
             messages.info(request, 'Chat został pomyślnie usunięty')
+            return redirect('chat_list')
+
+        if delete_confirmation.user2_conf == second_user:
+            room.delete()
+            messages.info(request, 'Chat został pomyślnie usunięty')
+            return redirect('chat_list')
+        return redirect('chat_list')
+
+    #if ask is just created
+    RoomDeleteAsk.objects.create(
+        room = room,
+        user1_ask = request.user
+    )
+    messages.info(request, f'{second_user} otrzyma wiadomość z zapytanie o potwierdzenie usunięcia chatu')
     return redirect('chat_list')
+
+
